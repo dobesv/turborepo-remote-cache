3404746f0670edcf39817a4b286122367822b126
CircleCI Build <dev@goformative.com>
2022-05-18 11:47:56 -0700

    Use built-in AWS credentials loading system

--- src/plugins/remote-cache/storage/s3.ts
+++ src/plugins/remote-cache/storage/s3.ts
@@ -2,25 +2,22 @@ import s3 from 's3-blob-store'
 import aws from 'aws-sdk'
 
 export interface S3Options {
-  accessKey?: string
-  secretKey?: string
-  region?: string
   endpoint?: string
   bucket: string
 }
-export function createS3({ accessKey, secretKey, bucket, region, endpoint }: S3Options) {
-  if (!accessKey || !secretKey || !(region || endpoint)) {
-    throw new Error(
-      `To use S3 storage "accessKey (S3_ACCESS_KEY)", "secretKey (S3_SECRET_KEY)", and one of "region (S3_REGION)" and "endpoint (S3_ENDPOINT)" parameters are mandatory.`,
-    )
+export function createS3({ bucket, endpoint }: S3Options) {
+  // Propagate "old" environment variables names to new ones for backwards compatibility
+  for (const [oldName, newName] of Object.entries({
+    S3_ACCESS_KEY: 'AWS_ACCESS_KEY_ID',
+    S3_SECRET_KEY: 'AWS_SECRET_ACCESS_KEY',
+    S3_REGION: 'AWS_REGION',
+  })) {
+    if (process.env[oldName] && !process.env[newName]) {
+      process.env[newName] = process.env[oldName]
+    }
   }
 
   const client = new aws.S3({
-    credentials: {
-      accessKeyId: accessKey,
-      secretAccessKey: secretKey,
-    },
-    ...(region ? { region } : {}),
     ...(endpoint ? { endpoint: new aws.Endpoint(endpoint) } : {}),
     ...(process.env.NODE_ENV === 'test' ? { sslEnabled: false, s3ForcePathStyle: true } : {}),
   })
