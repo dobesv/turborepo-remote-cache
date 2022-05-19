cd6647f2eed61f8eee51188b441f85fed2e3b1be
CircleCI Build <dev@goformative.com>
2022-05-19 12:39:41 -0700

    Don't try to support old environment variables for S3 credentials

--- src/plugins/remote-cache/storage/s3.ts
+++ src/plugins/remote-cache/storage/s3.ts
@@ -6,17 +6,6 @@ export interface S3Options {
   bucket: string
 }
 export function createS3({ bucket, endpoint }: S3Options) {
-  // Propagate "old" environment variables names to new ones for backwards compatibility
-  for (const [oldName, newName] of Object.entries({
-    S3_ACCESS_KEY: 'AWS_ACCESS_KEY_ID',
-    S3_SECRET_KEY: 'AWS_SECRET_ACCESS_KEY',
-    S3_REGION: 'AWS_REGION',
-  })) {
-    if (process.env[oldName] && !process.env[newName]) {
-      process.env[newName] = process.env[oldName]
-    }
-  }
-
   const client = new aws.S3({
     ...(endpoint ? { endpoint: new aws.Endpoint(endpoint) } : {}),
     ...(process.env.NODE_ENV === 'test' ? { sslEnabled: false, s3ForcePathStyle: true } : {}),
